03e2ca8d08ef5e3edcb84d7abf3cda0ee7433354
Alex <o.redka@smartgroup.ua>
2020-03-28 22:42:42 +0200

    Updates

---
+++ precache-manifest.e3ca7c02e03a621e7931406a534b925b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4339e80d67f1347926a163472f2cda4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f534ad8b86344bd335d5",
+    "url": "/goit-react-hw-02-feedback/static/css/main.f2c02658.chunk.css"
+  },
+  {
+    "revision": "3f93058c01f1687d3ade",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f6639ce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.f6639ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f534ad8b86344bd335d5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.668a2def.chunk.js"
+  },
+  {
+    "revision": "fc0b6e2e1527eed8558e",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.b60acdf3.js"
+  }
+]);
